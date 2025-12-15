94f5c7060dedb0a25c3d59ffd62797beb8567c92
manuko19 <manukotlik@gmail.com>
2025-12-15 12:57:31 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+// sw.js (Service Worker)
+
+const CACHE_NAME = 'marcacion-cache-v1';
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json'
+];
+
+// Instalación: Cargar recursos en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación: Limpiar cachés antiguos
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: Servir archivos desde la caché (esto es clave para el modo offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
